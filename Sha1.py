1e7f97041e17505ed5a714ff80af2070b006dea2
Shahana Rahman <rashariz@gmail.com>
2020-03-22 16:00:24 -0500

    new feature for less than and equal

--- Sha1.py
+++ Sha1.py
@@ -5,3 +5,11 @@ num2 = int(input('Enter your second number: '))
 #Display if num1 is > than num2
 if (num1 > num2):
     print('num1 is bigger than num2')
+
+#Display if num1 is < num2
+if (num1 < num2):
+    print('num1 is smaller than num2')
+
+#Display if they are equal
+if (num1 == num2):
+    print('num1 and num2 are equal')
