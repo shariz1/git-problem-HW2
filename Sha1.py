cbd33535d7a376c2309a6000d5ba6bdef6ea2070
Shahana Rahman <rashariz@gmail.com>
2020-03-22 15:35:19 -0500

    edit 1 for greater than

---
+++ Sha1.py
@@ -0,0 +1,7 @@
+#set up variables
+num1 = int(input('Enter your first number: '))
+num2 = int(input('Enter your second number: '))
+
+#Display if num1 is > than num2
+if (num1 > num2):
+    print('num1 is bigger than num2')
